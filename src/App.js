fad439dae94629c50b45c96e2a577d6a0e4077d0
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-04-26 21:55:22 +0300

    Integrate mapbox map through useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { render } from "react-dom";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 import "./index.css";
@@ -6,7 +6,16 @@ import "./index.css";
 function App() {
   const [random, setRandom] = useState(Math.random());
 
-  mapboxgl.accessToken = "ЗАМЕНИТЕ_НА_ПОЛУЧЕННЫЙ_ВАМИ_ТОКЕН";
+  mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWFraG1ldCIsImEiOiJjbDFxYTFnbGgwZm9iM2twdGFrZmIwbTF3In0.dHeMvTTWEoqLLM2Gu0SEyQ";
+
+  useLayoutEffect(() => { 
+    new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [37.611920, 55.761990],
+    zoom: 10
+  })
+}, [])
 
   return (
     <>
@@ -26,3 +35,4 @@ render(<App />, document.querySelector("#root"));
 
 
 export default App;
+
