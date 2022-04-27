a7e01389b0f57a06c7c50b2c9576d6f2adb44e3f
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-04-27 16:49:08 +0300

    Remove redundant createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
 import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+render((
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+), document.querySelector("#root"));
+
 
 
