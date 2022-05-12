d0806580b15ea6d2ec404937845d035af4933f19
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-05-12 17:40:33 +0300

    Update project for the new task

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@ import "./index.css";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
 function App() {
-  const [random, setRandom] = useState(Math.random());
+  const [marker, setMarker] = useState();
 
   mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWFraG1ldCIsImEiOiJjbDJoZjdybTAwZDllM2lwaGpwMjZreHhsIn0.VpRx5KdVw-9fAvWYptlseg";
 
   useLayoutEffect(() => { 
-    var map  = new mapboxgl.Map({
+    const map  = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [37.611920, 55.761990],
+    center: [37.610641, 55.761994],
     zoom: 10
   })
+
+    setMarker(new mapboxgl.Marker()
+    .setLngLat([37.6173, 55.7558])
+    .addTo(map))
   }, [])
+
+  const stores = {
+    km20: [37.610641, 55.761994],
+    belief: [37.601152, 55.733396],
+    brandshop: [37.616812, 55.767839]
+  };
 
   return (
     <>
-      <button
-        className="ui-button"
-        id="rerender"
-        onClick={() => setRandom(Math.random())}
-      >
-        Ререндер!
-      </button>
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select>
+          <option value="km20">KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
       <div id="map"></div>
     </>
   );
