41424d33949db053f3b60aff04bc5583e32ad0ae
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-05-16 16:37:54 +0300

    Add a new state and a handler for it

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
 function App() {
   const [marker, setMarker] = useState();
-
-  mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWFraG1ldCIsImEiOiJjbDJoZjdybTAwZDllM2lwaGpwMjZreHhsIn0.VpRx5KdVw-9fAvWYptlseg";
-
-  useLayoutEffect(() => { 
-    const map  = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [37.610641, 55.761994],
-    zoom: 10
-  })
-
-    setMarker(new mapboxgl.Marker()
-    .setLngLat([37.6173, 55.7558])
-    .addTo(map))
-  }, [])
+  const [store, setStore] = useState("km20");
 
   const stores = {
     km20: [37.610641, 55.761994],
@@ -26,11 +12,33 @@ function App() {
     brandshop: [37.616812, 55.767839]
   };
 
+  mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWFraG1ldCIsImEiOiJjbDJoZjdybTAwZDllM2lwaGpwMjZreHhsIn0.VpRx5KdVw-9fAvWYptlseg";
+
+  useLayoutEffect(() => {
+    const map  = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [37.610641, 55.761994],
+    zoom: 10
+    })
+
+    setMarker(new mapboxgl.Marker()
+    .setLngLat([37.6173, 55.7558])
+    .addTo(map)
+    )
+  }, [])
+
+  function handleStoreChange(e) {
+    setStore(e.target.value);
+    marker.setLNGat(stores[store]);
+
+  }
+
   return (
     <>
       <div className="map-overlay">
         <h3>Выберите магазин: </h3>
-        <select>
+        <select onChange={handleStoreChange}>
           <option value="km20">KM20</option>
           <option value="belief">BELIEF</option>
           <option value="brandshop">BRANDSHOP</option>
@@ -43,3 +51,4 @@ function App() {
 
 export default App;
 
+// посмотреть урок стейт в юзэффект
